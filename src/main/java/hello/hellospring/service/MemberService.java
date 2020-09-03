256490700032ad85586fc088c2a7d031ec82006b
nari-52 <60787793+nari-52@users.noreply.github.com>
2020-09-03 14:02:52 +0900

    20200903

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,60 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    // test 코드와 중복되지 않도록
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /*
+        회원가입 비즈니스 메소드 만들기
+    */
+    // 회원가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        // result.orElseGet(member1); // 값이 있으면 꺼내고 없으면 method 실행하라
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });*/
+
+        // 위와 같은 코드
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member); // 검증 통과 시 저장
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 한명의 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+
+
+
+}
